dbf77a35cb69c7376732533a2cfce158925bce5e
Camila <camila-brito10@hotmail.com>
2023-08-16 11:14:01 -0300

    modifications

--- Program.java
+++ Program.java
@@ -1,38 +1,39 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.util.List;
 
-import db.DB;
-import db.DbIntegrityException;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
+    public static void main(String[] args) {
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
-	public static void main(String[] args) {
-
-		Connection conn = null;
-		PreparedStatement st = null;
-		try {
-			conn = DB.getConnection();
-	
-			st = conn.prepareStatement(
-					"DELETE FROM department "
-					+ "WHERE "
-					+ "Id = ?");
-
-			st.setInt(1, 5);
-			
-			int rowsAffected = st.executeUpdate();
-			
-			System.out.println("Done! Rows affected: " + rowsAffected);
-		}
-		catch (SQLException e) {
-			throw new DbIntegrityException(e.getMessage());
-		} 
-		finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
-	}
+       
+       System.out.println("=== TEST:1 seller findByID ===");
+       Seller seller = sellerDao.findById(3);
+       System.out.println(seller);
+       
+       
+       
+       System.out.println("\n=== TEST:2 seller findByDepartment ===");
+      Department department = new Department(2, null);
+       List<Seller> list = sellerDao.findByDepartment(department);
+       for (Seller obj: list) {
+    	   System.out.println(obj);
+    	   
+    	   
+    	   System.out.println("=== TEST:3 seller findAll ===");
+    	   list = sellerDao.findAll();
+    	   for (Seller obj1: list) {
+        	   System.out.println(obj1);
+    	   
+       }
+    	   
+        }
+    }
 }
+
+
